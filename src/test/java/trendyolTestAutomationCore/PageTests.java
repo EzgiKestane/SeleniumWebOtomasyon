475f1c7a8d1f4b03e0a5e38010e3ef40a32271dc
Ezgi Kestane <36894508+EzgiKestane@users.noreply.github.com>
2020-07-08 17:26:49 +0300

    Searchbox used successfully

--- src/test/java/trendyolTestAutomationCore/PageTests.java
+++ src/test/java/trendyolTestAutomationCore/PageTests.java
@@ -5,6 +5,7 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -32,11 +33,25 @@ public class PageTests extends PageControl {
             System.out.println("No pop-up found");
         }
     }
+
     @Test
-    public void loginPageTest(){
+    public void loginPageTest() {
         driver.get("https://www.trendyol.com/User/Login");
         waitForPageLoad();
         System.out.println("Login site has been opened");
+    }
+
+    @Test
+    public void loginProcessTest() {
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"fEmailx\"]"))).click();
+            driver.findElement(By.id("fEmailx")).sendKeys("test.test12325@yahoo.com");
+            driver.findElement(By.id("LoginModel_Password")).sendKeys("Ckeck12345*");
+            driver.findElement(By.id("loginSubmitButton")).click();
+            System.out.println("You have been logged in");
+        } catch (Exception e) {
+            System.out.println("Login unsuccessful");
+        }
     }
 
     /*@Test
@@ -53,4 +68,27 @@ public class PageTests extends PageControl {
         }
     }*/
 
+    @Test
+    public void searchBoxTest() {
+        try {
+            waitForPageLoad();
+            if (driver.findElements(By.xpath("//*[@id=\"Combined-Shape\"]")).size() > 0) {
+                driver.findElement(By.xpath("//*[@id=\"Combined-Shape\"]")).click();
+            }
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.className("search-box")));
+            driver.findElement(By.className("search-box")).click();
+            waitForPageLoad();
+            driver.findElement(By.className("search-box")).sendKeys("bilgisayar");
+            driver.findElement(By.className("search-box")).sendKeys(Keys.ENTER);
+            System.out.println("Search done");
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"search-app\"]/div/div/div[2]/div[2]/div")));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Search failed.");
+        }
+
+    }
+
 }
+
