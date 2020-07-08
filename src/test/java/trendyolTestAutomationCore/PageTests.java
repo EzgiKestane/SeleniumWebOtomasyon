e6b1ab0da9f44afd44229b66eec562cc795f48f5
Ezgi Kestane <36894508+EzgiKestane@users.noreply.github.com>
2020-07-08 20:51:10 +0300

    Use-case test finished and comments added.

--- src/test/java/trendyolTestAutomationCore/PageTests.java
+++ src/test/java/trendyolTestAutomationCore/PageTests.java
@@ -19,7 +19,10 @@ import java.time.Duration;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PageTests extends PageControl {
 
-
+    /**
+     * Bu test ile sitenin title'ı ile girilen url'in title'ının eşitliği doğrulanır.Buna göre consola
+     * eşit olup olmama durumu yazdırılır.
+     */
     @Test
     public void a_titleTest() {
         driver.get(url);
@@ -35,14 +38,10 @@ public class PageTests extends PageControl {
         }
     }
 
-  /*  @Test
-    public void b_loginPageTest() {
-        driver.get("https://www.trendyol.com/User/Login");
-        waitForPageLoad();
-        System.out.println("Login site has been opened");
-    }
-
-   */
+    /**
+     * Bu test ile mouse'un giriş menusu üzerine hover edilmesi sağlanır. Ve açılan submenude
+     * Giriş Yap butonuna tıklandığının doğruluğu test edilir. Ve tıklanılırsa consola yazdırılır.
+     */
     @Test
     public void b_loginButtonTest() {
         try {
@@ -58,30 +57,31 @@ public class PageTests extends PageControl {
         }
     }
 
+    /**
+     * Bu test ile login pop-up'ında Email ve Şifre Girilerek Giriş Yap butonu tıklanılabilirliği doğrulanır.
+     * Ve consola yazdırılır.
+     */
     @Test
     public void c_loginProcessTest() {
         try {
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"fEmailx\"]"))).click();
-            driver.findElement(By.id("fEmailx")).sendKeys("test.test12325@yahoo.com");
-            driver.findElement(By.id("LoginModel_Password")).sendKeys("Ckeck12345*");
-            driver.findElement(By.id("loginSubmitButton")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"email\"]"))).click();
+            driver.findElement(By.id("email")).sendKeys("test.test12325@yahoo.com");
+            driver.findElement(By.id("password")).sendKeys("Ckeck12345*");
+            driver.findElement(By.id("loginSubmit")).click();
             System.out.println("You have been logged in");
         } catch (Exception e) {
             System.out.println("Login unsuccessful");
         }
     }
 
-  /*  @Test
+    /**
+     * Bu test ile arama kutusuna "bilgisayar" kelimesi yazdırılarak Enter tuşuna basılır ve arama yapılabilirliği
+     * test edilir. Ve consola yazdırılır.
+     */
+    @Test
     public void d_searchBoxTest() {
         try {
-            waitForPageLoad();
-            if (driver.findElements(By.xpath("//*[@id=\"Combined-Shape\"]")).size() > 0) {
-                driver.findElement(By.xpath("//*[@id=\"Combined-Shape\"]")).click();
-            }
-
             wait.until(ExpectedConditions.elementToBeClickable(By.className("search-box")));
-            driver.findElement(By.className("search-box")).click();
-            waitForPageLoad();
             driver.findElement(By.className("search-box")).sendKeys("bilgisayar");
             driver.findElement(By.className("search-box")).sendKeys(Keys.ENTER);
             System.out.println("Search done");
@@ -92,13 +92,113 @@ public class PageTests extends PageControl {
         }
 
     }
+
+    /**
+     * Bu test ile rastgele seçtiğim bir iteme tıklanılabilirlik test edilir.
+     */
     @Test
-    public void e_itemSelectTest(){
-        waitForPageLoad();
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"search-app\"]/div/div/div[2]/div[2]/div/div[1]")));
-        driver.findElement(By.xpath("//*[@id=\"search-app\"]/div/div/div[2]/div[2]/div/div[1]")).click();
-        waitForPageLoad();
+    public void e_itemSelectTest() {
+        try {
+            waitForPageLoad();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"search-app\"]/div/div/div[2]/div[2]/div/div[1]")));
+            driver.findElement(By.xpath("//*[@id=\"search-app\"]/div/div/div[2]/div[2]/div/div[1]")).click();
+            waitForPageLoad();
+        } catch (Exception e) {
+            System.out.println("You couldn't select an item.");
+        }
     }
-*/
+
+    /**
+     * Bu test ile sepete ekleme işleminin yapılabilirliği test edilir.
+     */
+    @Test
+    public void f_addToBasketTest() {
+        try {
+            driver.findElement(By.className("add-to-bs-tx")).click();
+            waitForPageLoad();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Item couldn't added to basket.");
+        }
+    }
+
+    /**
+     * Bu test ile ürün sayfasındaki fiyat ile pop-up sepetindeki ürün fiyatı karşılaştırılır ve eşitliği test
+     * edilir.
+     */
+    @Test
+    public void g_basketPriceTest() {
+        try {
+            String product_Price = driver.findElement(By.className("prc-slg")).getText();
+            System.out.println(product_Price);
+            WebElement we = driver.findElement(By.id("myBasketListItem"));
+            action.moveToElement(we).build().perform();
+            wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Sepete Git")));
+            String product_box_price = driver.findElement(By.className("salePrice")).getText();
+            System.out.println(product_box_price);
+            if (product_Price.equals(product_box_price)) {
+                System.out.println("Prices are equal.");
+            } else {
+                System.out.println("Prices are not equal.");
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+        }
+    }
+
+    /**
+     * Bu test ile sepete eklenen ürün '+' tıklanarak fonksiyonun çalıştığı kontrol edilir.
+     * Ve ürün sayısının 2'ye eşit olduğu test edilir ve consola yazdırılır.
+     */
+    @Test
+    public void h_productCountTest() {
+        try {
+            driver.findElement(By.xpath("//*[@id=\"myBasketListItem\"]")).click();
+            waitForPageLoad();
+            wait.until(ExpectedConditions.elementToBeClickable(By.className("ty-numeric-counter-button")));
+            driver.findElement(By.xpath("//*[@id=\"partial-basket\"]/div/div[3]/div[2]/div[3]/div[1]/div/button[2]")).click();
+            String productCount = driver.findElement(By.className("counter-content")).getText();
+            String productMomentCount = "2";
+            System.out.println(productCount);
+            if (productCount.equals(productMomentCount)) {
+                System.out.println(productCount);
+            } else {
+                System.out.println("Item count is not equal");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Bu test ile ürün çöp kutusuna tıklanarak silinebilirliği test edilir.
+     */
+    @Test
+    public void i_deletingItemTest() {
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(By.className("i-trash")));
+            driver.findElement(By.xpath("//*[@id=\"partial-basket\"]/div/div[2]/div[2]/div[3]/button")).click();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-item btn-remove")));
+            driver.findElement(By.xpath("//*[@id=\"ngdialog1\"]/div[2]/form/div/div[2]/div/div[1]/button[2]")).click();
+
+        } catch (Exception e) {
+            try {
+                driver.findElement(By.xpath("//*[@id=\"ngdialog1\"]/div[2]/form/div/div[2]/div/button[2]")).click();
+                waitForPageLoad();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+
+        } finally {
+            waitForPageLoad();
+            System.out.println("Items Deleted.");
+        }
+
+    }
+
 }
 
